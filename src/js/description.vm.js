7d108df8477a52777a247e57f0798db05476ea94
Antoine Chalifour <antoine.chalifour@gmail.com>
2015-04-28 23:19:37 +0200

    render description

--- src/js/description.vm.js
+++ src/js/description.vm.js
@@ -4,10 +4,41 @@
 
   function DescriptionViewModel(model){
     var self = this;
+    var srcTplDescription = app.templates['description'];
+    var srcTplActivity = app.templates['activity'];
 
     self.model = model;
+    self.currentIndex = null;
+    self.templateDescription = Handlebars.compile(srcTplDescription);
+    self.templateActivity = Handlebars.compile(srcTplActivity);
     self.$view = $('#description');
+
+    self.model.on('timetable:selected', self.render.bind(self));
   }
+
+  DescriptionViewModel.prototype.render = function(data){
+    var self = this;
+    var index = data.index;
+    if(self.currentIndex === index) return false;
+    self.currentIndex = index;
+
+    var current = self.model.table[index];
+    var binding = {
+      title: current.label,
+      description: current.description,
+      activities: current.activities
+    };
+
+    var html = self.templateDescription(binding);
+    self.$view
+      .hide()
+      .html(html)
+      .fadeIn();
+
+    self.$activityList = $('#activity-list');
+    self.$newActivityText = $('description-new-activity-text');
+    self.$newActivityHours = $('description-new-activity-hours');
+  };
 
   app.DescriptionViewModel = DescriptionViewModel;
 })(jQuery, window);
